d4a953fda5419652800b95e1b21108391339475c
root <root@THANANAN-DEV.AD.DAppmaker.com>
2026-05-19 17:33:11 +0700

    setup

---
+++ internal/feedback/feedback.go
@@ -0,0 +1,16 @@
+﻿package feedback
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type FeedbackDomain struct {
+}
+
+func NewFeedbackDomain(db *mongo.Database) *FeedbackDomain {
+	return &FeedbackDomain{}
+}
+
+func (d *FeedbackDomain) RegisterRoutes(app *fiber.App) {
+}
